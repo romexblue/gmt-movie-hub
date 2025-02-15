2712c32e6777ab1b2289c275a0cf34a1ec7d13f5
romexblue <gerometahud55@gmail.com>
2025-02-15 23:27:54 +0800

    Refractored SearchCard

---
+++ src/components/SearchCard/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Image from "next/image";
+
+import { TMDBMovie } from "~/lib/types";
+import { getMovieEmoji, getMovieTitle } from "~/lib/utils";
+
+const IMAGE_LINK = process.env.NEXT_PUBLIC_TMDB_IMAGE_LINK;
+
+type Props = {
+    movie: TMDBMovie;
+    isTwoColumnLayout: boolean;
+};
+
+const SearchCard = ({ movie, isTwoColumnLayout }: Props) => {
+    const title = getMovieTitle(movie);
+    const movieEmoji = getMovieEmoji(movie);
+    return (
+        <div
+            key={movie?.id}
+            className={`flex gap-4 items-center ${
+                isTwoColumnLayout ? "max-w-[31.25rem]" : "w-full"
+            }`}
+        >
+            <div className="relative rounded-xl h-40 w-28 min-w-28 overflow-hidden">
+                {movie?.poster_path ? (
+                    <Image
+                        alt={title}
+                        fill
+                        className="object-cover"
+                        src={`${IMAGE_LINK}${movie?.poster_path}`}
+                        sizes="(max-width: 640px) 30vw, (max-width: 1024px) 15vw, 10vw"
+                    />
+                ) : (
+                    <div className="flex h-full w-full items-center justify-center bg-gray-700 text-gray-400 text-sm text-center">
+                        No Image Available
+                    </div>
+                )}
+            </div>
+            <div>
+                <h3 className="text-lg font-bold">{title}</h3>
+                <span className="capitalize text-gray-300">
+                    {movieEmoji}
+                    {movie?.media_type}
+                </span>
+                {movie?.release_date && (
+                    <p className="text-gray-300">📅 {movie.release_date}</p>
+                )}
+                <p className="text-gray-400">⭐ {movie?.vote_average}/10</p>
+                <p className="text-gray-400 line-clamp-2">{movie?.overview}</p>
+            </div>
+        </div>
+    );
+};
+
+export default SearchCard;
--- src/components/SearchCardList/index.tsx
+++ src/components/SearchCardList/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Image from "next/image";
 
 import Loading from "~/components/Loading";
 import Error from "~/components/Error";
-import { getMovieEmoji, getMovieTitle } from "~/lib/utils";
+import SearchCard from "~/components/SearchCard";
 import { TMDBMovie } from "~/lib/types";
-
-const IMAGE_LINK = process.env.NEXT_PUBLIC_TMDB_IMAGE_LINK;
 
 type Props = {
     movieList: TMDBMovie[] | undefined;
@@ -35,57 +32,13 @@ const SearchCardList = ({ movieList = [], isLoading, isError }: Props) => {
                             : "grid-cols-1"
                     }`}
                 >
-                    {movieList.map((movie) => {
-                        const title = getMovieTitle(movie);
-                        const movieEmoji = getMovieEmoji(movie);
-
-                        return (
-                            <div
-                                key={movie?.id}
-                                className={`flex gap-4 items-center ${
-                                    isTwoColumnLayout
-                                        ? "max-w-[31.25rem]"
-                                        : "w-full"
-                                }`}
-                            >
-                                <div className="relative rounded-xl h-40 w-28 min-w-28 overflow-hidden">
-                                    {movie?.poster_path ? (
-                                        <Image
-                                            alt={title}
-                                            fill
-                                            className="object-cover"
-                                            src={`${IMAGE_LINK}${movie?.poster_path}`}
-                                            sizes="(max-width: 640px) 30vw, (max-width: 1024px) 15vw, 10vw"
-                                        />
-                                    ) : (
-                                        <div className="flex h-full w-full items-center justify-center bg-gray-700 text-gray-400 text-sm text-center">
-                                            No Image Available
-                                        </div>
-                                    )}
-                                </div>
-                                <div>
-                                    <h3 className="text-lg font-bold">
-                                        {title}
-                                    </h3>
-                                    <span className="capitalize text-gray-300">
-                                        {movieEmoji}
-                                        {movie?.media_type}
-                                    </span>
-                                    {movie?.release_date && (
-                                        <p className="text-gray-300">
-                                            📅 {movie.release_date}
-                                        </p>
-                                    )}
-                                    <p className="text-gray-400">
-                                        ⭐ {movie?.vote_average}/10
-                                    </p>
-                                    <p className="text-gray-400 line-clamp-2">
-                                        {movie?.overview}
-                                    </p>
-                                </div>
-                            </div>
-                        );
-                    })}
+                    {movieList.map((movie) => (
+                        <SearchCard
+                            key={movie?.id}
+                            movie={movie}
+                            isTwoColumnLayout={isTwoColumnLayout}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
